aaebe1d851e01baaa2847151468b35fb6ee1ef86
BecaLParker <becaparker@gmail.com>
2021-05-20 16:20:23 +0100

    Wrapping Database Data in Program Objects

--- spec/features/adding_bookmarks_spec.rb
+++ spec/features/adding_bookmarks_spec.rb
@@ -4,17 +4,11 @@ require './app'
 
 feature 'adding a bookmark' do
   scenario 'a user can add new bookmark' do
-    # connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    # connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    # connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.google.com');")
-    # connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.destroyallsoftware.com');")
-
     visit('/bookmarks/new')
     fill_in('url', with: 'http://testbookmark.com')
+    fill_in('title', with: 'Test Bookmark')
     click_button('Submit')
 
-    expect(page).to have_content 'http://testbookmark.com'
- 
+    expect(page).to have_link('Test Bookmark', href: 'http://testbookmark.com')
   end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -4,15 +4,14 @@ require './app'
 
 feature 'viewing bookmarks' do
   scenario 'shows a list of bookmarks' do
-
-    Bookmark.create(url: "http://www.makersacademy.com")
-    Bookmark.create(url: "http://www.destroyallsoftware.com")
-    Bookmark.create(url: "http://www.google.com")
+    Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    Bookmark.create(url: 'http://www.destroyallsoftware.com', title: 'DEST')
+    Bookmark.create(url: 'http://www.google.com', title: 'Google')
 
     visit('/bookmarks')
 
-    expect(page).to have_content 'http://www.makersacademy.com'
-    expect(page).to have_content 'http://www.google.com'
-    expect(page).to have_content 'http://www.destroyallsoftware.com'
+    expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+    expect(page).to have_link('Google', href: 'http://www.google.com')
+    expect(page).to have_link('DEST', href: 'http://www.destroyallsoftware.com')
   end
 end
